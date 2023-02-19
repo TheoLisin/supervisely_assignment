267117e5d933a35405cef254deea2155457c1903
Theo <theo.lisin@gmail.com>
2023-02-19 17:03:20 +0300

    added path to images

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,3 +13,13 @@ def tmp_path(tmp_path_factory) -> Path:
     """Temporary path for any data"""
     tmp_dir = tmp_path_factory.mktemp("tmp")
     return tmp_dir
+
+
+@pytest.fixture
+def lena_path(assets: Path) -> Path:
+    return assets / "lena.jpg"
+
+
+@pytest.fixture
+def gslena_path(assets: Path) -> Path:
+    return assets / "gslena.jpg"
