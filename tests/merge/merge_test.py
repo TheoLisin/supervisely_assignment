4b370f7ceac89ab730411cd63ad23eb172993bc4
Theo <theo.lisin@gmail.com>
2023-02-19 17:04:02 +0300

    added tests for different shapes; added Errors test

--- tests/merge/merge_test.py
+++ tests/merge/merge_test.py
@@ -1,4 +1,9 @@
-from module.scripts.splitmerge.merge import _get_params, SliceParams
+import pytest
+from pathlib import Path
+from typing import Tuple
+
+from module.scripts.splitmerge.split import split_image
+from module.scripts.splitmerge.merge import _get_params, SliceParams, merge_image
 
 
 def test_simple_get_params():
@@ -10,3 +15,53 @@ def test_simple_get_params():
     }
 
     assert _get_params(name) == SliceParams(**ans)
+
+
+@pytest.mark.parametrize(
+    ("win_shape", "shifts"),
+    [
+        ((32, 32), (32, 32)),
+        ((32, 64), (17, 23)),
+        ((800, 800), (3, 3)),
+    ],
+)
+def test_merge_diff_shapes(
+    win_shape: Tuple[int, int],
+    shifts: Tuple[int, int],
+    lena_path: Path,
+    tmp_path: Path,
+):
+    split_image(lena_path, win_shape, shifts, tmp_path)
+    merge_image(tmp_path / "lena", tmp_path, lena_path)
+
+
+@pytest.mark.parametrize(
+    ("win_shape", "shifts"),
+    [
+        ((32, 32), (32, 32)),
+        ((32, 64), (17, 23)),
+        ((800, 800), (3, 3)),
+    ],
+)
+def test_merge_diff_shapes_one_channel(
+    win_shape: Tuple[int, int],
+    shifts: Tuple[int, int],
+    gslena_path: Path,
+    tmp_path: Path,
+):
+    split_image(gslena_path, win_shape, shifts, tmp_path)
+    merge_image(tmp_path / "gslena", tmp_path, gslena_path)
+
+
+def test_merge_wrong_shapes(
+    lena_path: Path,
+    tmp_path: Path,
+):
+    win_shape = (32, 32)
+    shifts = (64, 64)
+    split_image(lena_path, win_shape, shifts, tmp_path)
+
+    with pytest.raises(
+        ValueError, match="Can't restore image: shift step is bigger then slice window.",
+    ):
+        merge_image(tmp_path / "lena", tmp_path, lena_path)
